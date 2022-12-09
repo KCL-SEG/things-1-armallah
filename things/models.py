8f65c3ca06aa14300ad8d4c5d3128fcab35445f9
Abdul Rahman <armallah1978@gmail.com>
2022-12-09 00:27:37 +0000

    exercise 3

--- things/models.py
+++ things/models.py
@@ -2,23 +2,21 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import *
+from django.core.validators import MinValueValidator 
+from django.core.validators import MaxValueValidator 
 
 
 class Thing(AbstractUser):
+    username = None
     
-    name = models.CharField(max_length=30, blank=False)
+    name = models.CharField(unique=True,max_length=30, blank=False)
     
-    quantity = models.IntegerField(
-        default=1,
-        validators=[
-            MaxValueValidator(100),
-            MinValueValidator(1)
-            ]
-        ,blank=False)
+    quantity = models.PositiveIntegerField(default=1,blank=False, validators=[MinValueValidator(1), MaxValueValidator(100)])
     
-    desc =  models.CharField(max_length=120, blank=True, unique=False)
+    desc =  models.CharField(max_length=120, blank=True)
     
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = ['name']
 
 
 # Create your models here.
