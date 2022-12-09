e46ad78bf36726c9bdb9fa67b0321c6826fa2750
Abdul Rahman <armallah1978@gmail.com>
2022-12-09 00:08:51 +0000

    exercise 1

--- things/models.py
+++ things/models.py
@@ -8,15 +8,7 @@ from django.core.validators import *
 class Thing(AbstractUser):
     
     name = models.CharField(max_length=30, blank=False)
-    
-    quantity = models.IntegerField(
-        default=1,
-        validators=[
-            MaxValueValidator(100),
-            MinValueValidator(1)
-            ]
-        ,blank=False)
-    
+    quantity = models.IntegerField(default=1,blank=False)
     desc =  models.CharField(max_length=120, blank=True, unique=False)
     
 
